4e71ab621ef1d193c1b4830997ac244bd55967db
noahmizuta <noahmizuta@gmail.com>
2023-02-05 20:40:36 +0900

    imply enemy class

--- Class1.cs
+++ Class1.cs
@@ -68,7 +68,217 @@ namespace WinFormApp1
                     _intTokuten = 500;
                     break;
             }
+        }
+        public void MvEnm()
+        {
+            switch (_intMovePattern)
+            {
+                case 1:
+                    var intMv = _rnd.Next(1, 5);
+                    switch (intMv)
+                    {
+                        case 1:
+                            if ((_pbImgEnemy.Top + _pbImgEnemy.Height+_intMvwd) < _enemyOwner.Height)
+                            {
+                                _pbImgEnemy.Top += _intMvwd;
+                            }
+                            break;
+                        case 2:
+                            if ((_pbImgEnemy.Top - _intMvwd) > 0)
+                            {
+                                _pbImgEnemy.Top -= _intMvwd;
+                            }
+                            break;
+                        case 3:
+                            if ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvud) < _enemyOwner.Width)
+                            {
+                                _pbImgEnemy.Left += _intMvud;
+                            }
+                            break;
+                        case 4:
+                            if ((_pbImgEnemy.Left - _intMvud) > 0)
+                            {
+                                _pbImgEnemy.Left -= _intMvud;
+                            }
+                            break;
+                    }
+                    break;
+                case 2:
+                    if (_intMvud == 0)
+                    {
+                        _intMvud = _rnd.Next(1, 5);
+                    }
+                    switch (_intMvud)
+                    {
+                        case 1:
+                            if ((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) < _enemyOwner.Height)
+                            {
+                                _pbImgEnemy.Top += _intMvwd;
+                            } 
+                            else
+                            {
+                                _intMvud = _rnd.Next(1, 5);
+                            }
+                            break;
+                        case 2:
+                            if ((_pbImgEnemy.Top - _intMvwd) > 0)
+                            {
+                                _pbImgEnemy.Top -= _intMvwd;
+                            }
+                            else
+                            {
+                                _intMvud = _rnd.Next(1, 5);
+                            }
+                            break;
+                        case 3:
+                            if ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) < _enemyOwner.Width)
+                            {
+                                _pbImgEnemy.Left += _intMvwd;
+                            }
+                            else
+                            {
+                                _intMvud = _rnd.Next(1, 5);
+                            }
+                            break;
+                        case 4:
+                            if ((_pbImgEnemy.Left - _intMvwd) > 0){
+                                _pbImgEnemy.Left -= _intMvwd;
+                            }
+                            else
+                            {
+                                _intMvud = _rnd.Next(1, 5);
+                            }
+                            break;
 
+                    }
+                    break;
+                case 3:
+                    if (_intMvud == 0)
+                    {
+                        _intMvud = _rnd.Next(1, 5);
+                    }
+                    switch (_intMvud)
+                    {
+                        case 1:
+                            //右下に進みたい
+                            //if ((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) < _enemyOwner.Height)
+                            //{
+                            //    _pbImgEnemy.Top += _intMvwd;
+                            //} 
+                            //else
+                            //{
+                            //    _intMvud = _rnd.Next(1, 5);
+                            //}
+
+                            if (((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) < _enemyOwner.Height) && (_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) < _enemyOwner.Width)
+                           　//右下に進める場合
+                            {
+                                _pbImgEnemy.Top += _intMvwd;
+                                _pbImgEnemy.Left += _intMvwd;
+                            }
+                            else //右下に進めない場合
+                            {
+                                if ((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) >= _enemyOwner.Height)
+                                //下が引っかかる場合
+                                {
+                                    _intMvud = 2;//右上に進む
+                                }
+                                else if ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) >= _enemyOwner.Width)
+                                //右が引っかかる場合
+                                {
+                                    _intMvud = 3;//左下に進む
+                                }
+                                else if (((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) >= _enemyOwner.Height) && ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) >= _enemyOwner.Width))
+                                {
+                                    //どっちも引っかかる場合　　　　　
+                                    _intMvud = 4; //左上に進む
+                                }
+                            }
+                            break;
+                        case 2:
+                            //右上に進みたい
+                            if (((_pbImgEnemy.Top - _intMvwd) > 0) && (_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) < _enemyOwner.Width)
+                           　//右上に進める場合
+                            {
+                                _pbImgEnemy.Top -= _intMvwd;
+                                _pbImgEnemy.Left += _intMvwd;
+                            }
+                            else //右上に進めない場合
+                            {
+                                if ((_pbImgEnemy.Top - _intMvwd) <= 0)
+                                //上が引っかかる場合
+                                {
+                                    _intMvud = 1;//右下に進む
+                                }
+                                else if ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) >= _enemyOwner.Width)
+                                //右が引っかかる場合
+                                {
+                                    _intMvud = 4;//左上に進む
+                                }
+                                else if (((_pbImgEnemy.Top - _intMvwd) <= 0) && ((_pbImgEnemy.Left + _pbImgEnemy.Width + _intMvwd) >= _enemyOwner.Width))
+                                {
+                                    //どっちも引っかかる場合　　　　　
+                                    _intMvud = 3; //左下に進む
+                                }
+                            }
+                            break;
+                        case 3:
+                            //左下に進みたい
+                            if (((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) < _enemyOwner.Height) && ((_pbImgEnemy.Left - _intMvwd) > 0))
+                           　//左下に進める場合
+                            {
+                                _pbImgEnemy.Top += _intMvwd;
+                                _pbImgEnemy.Left -= _intMvwd;
+                            }
+                            else //左下に進めない場合
+                            {
+                                if ((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) >= _enemyOwner.Height)
+                                //下が引っかかる場合
+                                {
+                                    _intMvud = 4;//左上に進む
+                                }
+                                else if ((_pbImgEnemy.Left - _intMvwd) <= 0)
+                                //左が引っかかる場合
+                                {
+                                    _intMvud = 1;//右下に進む
+                                }
+                                else if (((_pbImgEnemy.Top + _pbImgEnemy.Height + _intMvwd) >= _enemyOwner.Height) && ((_pbImgEnemy.Left - _intMvwd) <= 0))
+                                {
+                                    //どっちも引っかかる場合　　　　　
+                                    _intMvud = 2; //右上に進む
+                                }
+                            }
+                            break;
+                        case 4:
+                            if (((_pbImgEnemy.Top - _intMvwd) > 0) && ((_pbImgEnemy.Left - _intMvwd) > 0))
+                           　//左上に進める場合
+                            {
+                                _pbImgEnemy.Top -= _intMvwd;
+                                _pbImgEnemy.Left -= _intMvwd;
+                            }
+                            else //左上に進めない場合
+                            {
+                                if ((_pbImgEnemy.Top - _intMvwd) <= 0)
+                                //上が引っかかる場合
+                                {
+                                    _intMvud = 3;//左下に進む
+                                }
+                                else if ((_pbImgEnemy.Left - _intMvwd) <= 0)
+                                //左が引っかかる場合
+                                {
+                                    _intMvud = 2;//右上に進む
+                                }
+                                else if (((_pbImgEnemy.Top - _intMvwd) <= 0) && ((_pbImgEnemy.Left - _intMvwd) <= 0))
+                                {
+                                    //どっちも引っかかる場合　　　　　
+                                    _intMvud = 1; //右下に進む
+                                }
+                            }
+                            break;
+                    }
+               break;
+
+            }
         }
     }
 }
